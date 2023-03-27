81b305f4e56b5b375f23661b90b3bf37d5d3c339
stbpiza <stbpiza@gmail.com>
2023-03-27 22:18:47 +0900

    프로그래머스 문제 풀이 https://school.programmers.co.kr/learn/courses/30/lessons/159993 미로탈출(아직안풀었음)

---
+++ coding/src/com/coding/programmers/level2/미로탈출.java
@@ -0,0 +1,23 @@
+package com.coding.programmers.level2;
+
+public class 미로탈출 {
+
+    public static void main(String[] args) {
+
+        // https://school.programmers.co.kr/learn/courses/30/lessons/159993
+
+        // 인풋데이터
+        String[] maps = {"SOOOL","XXXXO","OOOOO","OXXXX","OOOOE"};
+
+        // 결과출력
+        System.out.println(solution(maps));
+
+    }
+
+    public static int solution(String[] maps) {
+        int answer = 0;
+
+        return answer;
+    }
+
+}
